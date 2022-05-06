d69b7f0223190528071dd4d241d41f76791603d8
AntonioMinkowski <antonio1384minkowski@zohomail.eu>
2022-05-06 23:57:13 +0430

    bugs are fixed

--- Client/train_module/trainer.py
+++ Client/train_module/trainer.py
@@ -1,23 +1,81 @@
 import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
 import socket
-import os 
+import os
+from config_loader.config_loader import load
 
-# This trainer module uses k-fold method to train the model
+# Create the history logger function
+# This function plot the network_history object and save it in .png format in log/ folder
+def train_history_logger(network_history, log_path):
+    history = network_history.history
+
+    # Extracting losses and accuracy from history
+    losses = history['loss']
+    validation_loss = history['val_loss']
+    accuracies = history['accuracy']
+    validation_accuracies = history['val_accuracy']
+
+    # Plot losses and validation losses per each epochs and saving the figure in log folder
+    plt.figure()
+    plt.grid()
+    plt.xlabel('Epochs')
+    plt.xlabel('Loss')
+    plt.plot(losses)
+    plt.plot(validation_loss)
+    plt.legend(['loss', 'validation_loss'])
+    plt.savefig(f'{log_path}/history_of_losses.png')
+
+    # Plot accuracy and validation accuracy per each epochs and saving the figure in log folder
+    plt.figure()
+    plt.grid()
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.plot(accuracies)
+    plt.plot(validation_accuracies)
+    plt.legend(['accuracy', 'validation_accuracy'])
+    plt.savefig(f'{log_path}//history_of_accuracies.png')
+
 # Create our trainer
-def train(model, epochs, batch_size):
-    model_path = os.getcwd() + '/' + socket.gethostname() + '.h5'
+def train(X_train, Y_train, X_test, Y_test):
+    try:
+        model_path = f'{os.getcwd()}/model/{socket.gethostname()}.h5'
 
-    # Make sure the name of model's file we want to train is exists
-    if not os.path.exists(model_path):
-        return F'{socket.gethostname()}.h5 is not exist!'
+        # Make sure the name of model's file we want to train is exists
+        if not os.path.exists(model_path):
+            return f'{socket.gethostname()}.h5 is not exist!'
 
-    # Load the model file
-    model = tf.keras.models.load_model(model_path)
+        # Logs and Callbacks
+        log_path = os.getcwd() + '/log'
+        if not os.path.exists(log_path):
+            os.mkdir('log')
 
-    # Logs and Callbacks
-    log_path = os.getcwd() + 'logs'
-    if not os.path.exists(log_path):
-        os.mkdir('log')
+        # Extract train parameters
+        num_epochs, batch_size, optimizer, loss_func = load()
 
-    model_checkpoint = tf.keras.callbacks.ModelCheckpoint(log_path + '/' + socket.gethostname() + '_ModelWeight.h5')
-    logger = tf.keras.callbacks.CSVLogger(log_path + '/' + socket.gethostname() + '_ModelTrainingLog.h5')
+        # Create our callbacks
+        model_checkpoint = tf.keras.callbacks.ModelCheckpoint(f'{log_path}/{socket.gethostname()}_ModelWeight.h5')
+        logger = tf.keras.callbacks.CSVLogger(f'{log_path}/{socket.gethostname()}_ModelTrainingLog.log')
+
+        callbacks = [model_checkpoint, logger]
+
+        # Load the model
+        model = tf.keras.models.load_model(model_path)
+
+        # Compile the model
+        model.compile(optimizer=optimizer, loss=loss_func, metrics=['accuracy'])
+        
+        # Start training
+        network_history = model.fit(X_train, Y_train,
+                                    epochs=num_epochs,
+                                    batch_size=batch_size,
+                                    verbose=1,
+                                    callbacks=callbacks,
+                                    validation_data=(X_test, Y_test))
+
+        # Plot newwork_history
+        train_history_logger(network_history, log_path)
+
+    except Exception as e:
+        with open(f'{log_path}/training_exception.log', 'w') as f:
+            f.write(str(e))
