d14c70f5fe41da958a18d6d484b8ec5e578447cb
AntonioMinkowski <antonio1384minkowski@zohomail.eu>
2022-05-06 23:58:19 +0430

    bugs are fixed

--- Server/FTP_Server/FTP.py
+++ Server/FTP_Server/FTP.py
@@ -1,183 +1,94 @@
-import socket 
 import os 
-import tqdm 
 
-# This FTP module will send model file in .h5 format and config thats includes num of epochs, 
+# This FTP module will send model file in .h5 format and config thats include number of epochs, 
 # batch_size, optimizer algorithm and loss function.
-# also Reciver function will recive trained model file in .h5 format and training logs in .zip format.
+# also Receiver function will receive trained model file in .h5 format and training logs in .zip format.
 
-# Create our Sender function
-def FTP_Sender(model_path, config_path, conn, addr):
-    # Initialize main FTP Server variables
-    # receive 4096 bytes each time
-    BUFFER_SIZE = 4096
-    FORMAT      = 'utf-8'
+# Create the Sender function
+def FTP_Sender(conn, path, data=None):
+    print('Wating for command.')
 
-    # Make sure the name of model file we want to send is exists
-    if not os.path.exists(model_path):
-        return f'{model_path} Not found!'
+    # Make sure the path of file we wanna send, exists
+    if not os.path.exists(path):
+        return f'{path} Not found!'
 
-    print(f'[FTP] Sending model file to {addr}')
+    # Extract file name from file path
+    filename = os.path.basename(path)
 
-    # ============== Start sending model file ============== 
-    # Get the model file size
-    filesize = os.path.getsize(model_path)
-
-    # Send model filesize
-    conn.send(str(filesize).encode(FORMAT))
-
-    # Start sending the model file
-    progress = tqdm.tqdm(range(filesize), f"Sending {model_path}", unit="B", unit_scale=True, unit_divisor=1024)
-    print(f'[START TRANSFER PROCESS] Sending model to {addr}')
-    with open(model_path, "rb") as f:
-        while True:
-            # Read the bytes from the file
-            bytes_read = f.read(BUFFER_SIZE)
-
-            if not bytes_read:
-                # File transmitting is done
-                break
-
-            # We use sendall to assure transimission in busy networks
-            conn.sendall(bytes_read)
-
-            # Update the progress bar
-            progress.update(len(bytes_read))
-
-    print(f'Model file was sended to {addr}')
-
-    # ============== Start sending config file ============== 
-    # Get the config file size
-    filesize = os.path.getsize(config_path)
-
-    # Send config filesize
-    conn.send(str(filesize).encode(FORMAT))
-
-    # Start sending the config file
-    progress = tqdm.tqdm(range(filesize), f"Sending {config_path}", unit="B", unit_scale=True, unit_divisor=1024)
-    print(f'[START TRANSFER PROCESS] Sending model to {addr}')
-    with open(config_path, "rb") as f:
-        while True:
-            # Read the bytes from the file
-            bytes_read = f.read(BUFFER_SIZE)
-
-            if not bytes_read:
-                # File transmitting is done
-                break
-
-            # We use sendall to assure transimission in busy networks
-            conn.sendall(bytes_read)
-
-            # Update the progress bar
-            progress.update(len(bytes_read))
-
-    print(f'Config file was sended to {addr}')
-
-    return True
-
-# Create our Reciver function
-def FTP_Reciver(conn, addr):
-    # Initialize main FTP Server variables
-    # Receive 4096 bytes each time
-    BUFFER_SIZE = 4096
-    SEPARATOR   = "<SEPARATOR>"
-    FORMAT      = 'utf-8'
-
-    # ============== Start reciving model file ============== 
-    # Receive the trained model file infos
-    # Receive using client socket, not server socket
-    received = conn.recv(BUFFER_SIZE).decode(FORMAT)
-    filename, filesize = received.split(SEPARATOR)
-
-    # Remove absolute path if there is
-    filename = os.path.basename(filename)
-
-    # Convert to integer
-    filesize = int(filesize)
-    print(f'[START RECIVING] FileName: {filename}, Size: {filesize}')
-
-    # Start receiving the trained model file from the socket and writing to the file stream
-    progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-    with open(filename, "wb") as f:
-        while True:
-            # Read 1024 bytes from the socket (receive)
-            bytes_read = conn.recv(BUFFER_SIZE)
-
-            if not bytes_read:    
-                # Nothing is received
-                # File transmitting is done
-                break
-
-            # Write to the trained model file the bytes we just received
-            f.write(bytes_read)
-
-            # Update the progress bar
-            progress.update(len(bytes_read))
-
-    print(f'{filename} recived from {addr}')
-
-    # ============== Start training logs file ============== 
-    # Receive the training logs file infos
-    # Receive using client socket, not server socket
-    received = conn.recv(BUFFER_SIZE).decode(FORMAT)
-    filename, filesize = received.split(SEPARATOR)
-
-    # Remove absolute path if there is
-    filename = os.path.basename(filename)
-
-    # Convert to integer
-    filesize = int(filesize)
-    print(f'[START RECIVING] FileName: {filename}, Size: {filesize}')
-
-    # Start receiving the training logs file from the socket and writing to the file stream
-    progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-    with open(filename, "wb") as f:
-        while True:
-            # Read 1024 bytes from the socket (receive)
-            bytes_read = conn.recv(BUFFER_SIZE)
-
-            if not bytes_read:    
-                # Nothing is received
-                # File transmitting is done
-                break
-
-            # Write to the training logs file the bytes we just received
-            f.write(bytes_read)
-
-            # Update the progress bar
-            progress.update(len(bytes_read))
-
-    print(f'{filename} recived from {addr}')
-
-# Create the DataSet Sender function
-def Dataset2Client(conn, addr, bytes, dataset_name):
-    # Initialize main FTP Server variables
-    # receive 4096 bytes each time
-    BUFFER_SIZE = 4096
-    FORMAT      = 'utf-8'
-
-    print(f'[FTP] Sending {dataset_name} dataset to {addr}')
-
-    # ============== Start sending model file ============== 
-    # Send dataset size
-    conn.send(str(len(bytes)).encode(FORMAT))
-
-    # Start sending the model file
-    progress = tqdm.tqdm(range(len(bytes)), f"Sending {dataset_name}", unit="B", unit_scale=True, unit_divisor=1024)
-    print(f'[START TRANSFER PROCESS] Sending model to {addr}')
+    # Create the Sender loop
     while True:
-        batches = []
-        for offset in range(0, len(bytes), BUFFER_SIZE):
-            bytes_read = bytes[offset: offset + BUFFER_SIZE]
+        # Wait for command
+        cmd = conn.recv(32).decode('utf-8')
 
-            if not bytes_read:
-                # File transmitting is done
-                break
+        # Sending the file's name
+        if cmd == 'getfilename':
+            print('"getfilename" command received.')
+            conn.sendall(filename.encode('utf-8'))
 
-            # We use sendall to assure transimission in busy networks
-            conn.sendall(bytes_read)
+        # Start sending the file
+        if cmd == 'getfile':
+            print('"getfile" command received. Going to send file.')
 
-            # Update the progress bar
-            progress.update(len(bytes_read))
+            if not data:
+                with open(path, 'rb') as f:
+                    data = f.read()
 
-    print(f'Model file was sended to {addr}')
+            conn.sendall(f"{'%16d' % len(data)}".encode('utf-8'))
+            conn.sendall(data)
+            print('File transmission done.')
+
+        # Breaking the loop when file transmission is completed.
+        if cmd == 'end':
+            print('"end" command received. Teminate.')
+            break
+
+# Create the Receiver function
+def FTP_Receiver(conn):
+    # Initializing pathes variables
+    log_path = os.getcwd() + '/log'
+    pre_trained_path = os.getcwd() + '/pre_trained_models'
+
+    print('Going to receive file.')
+
+    # Sending the "getfilename" command and waiting to receiving the filename
+    conn.sendall('getfilename'.encode('utf-8'))
+    filename = conn.recv(1024).decode('utf-8')
+
+    print(f'============={filename}============')
+
+    # Make sure the needed pathes are existing
+    if not os.path.exists(log_path):
+        os.mkdir('log')
+
+    if not os.path.exists(pre_trained_path):
+        os.mkdir('pre_trained_models')
+
+    # Separating save path by formates
+    if filename.endswith('.h5'):
+        save_path = f'{pre_trained_path}/{filename}'
+
+    else:
+        save_path = f'{log_path}/{filename}'
+    
+    print(save_path)
+
+    print('Filename: ' + filename)
+
+    # Start receiving and writing file
+    with open(save_path, 'wb') as f:
+        while True:
+            conn.sendall('getfile'.encode('utf-8'))
+            size = int(conn.recv(16).decode('utf-8'))
+            print('Total size: ' + str(size))
+            recvd = b''
+            while size > len(recvd):
+                data = conn.recv(1024)
+                if not data: 
+                    break
+                recvd += data
+                f.write(data)
+            break
+
+    # End of Receiving
+    conn.sendall('end'.encode('utf-8'))
+    print('File received.')
